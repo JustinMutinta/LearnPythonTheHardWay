ee103d7b889e56bde94b58ef8b0263957b867c14
Justin Mutinta <justinmutinta@hotmail.com>
2022-04-01 22:35:47 -0500

    Lists

---
+++ ex45.py
@@ -0,0 +1,10 @@
+superhero_list = ["Spider-man",
+                    "Superman",
+                    "Batman",
+                    "Catwoman",
+                    "Catman",
+                    "Cyclops",
+                    "Wolverine",
+                    "Rogue"]
+
+print(superhero_list[4])
